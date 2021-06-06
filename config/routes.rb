4522d5d47451c2ff2f7d6bd4318fd912ddc374cb
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-06-06 20:21:07 +1000

    Coupon creator created

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :coupons, only: [:index, :new, :create, :show]
+  
 end
